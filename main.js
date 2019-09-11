5d52a3322269f5be7046d0357d2df3c7c54419e7
João Viana <joaoviana@Joaos-MacBook-Pro.local>
2019-09-11 15:15:23 +0100

    client side validation on click of button 'test'

--- main.js
+++ main.js
@@ -1,21 +1,21 @@
 document.getElementById('password').addEventListener("input", (event) => {
     let messagesContainer = document.getElementById('messages');
     messagesContainer.innerHTML = '';
-    let firstName = document.getElementById('firstname').value;
-    let lastName = document.getElementById('lastname').value;
+    // let firstName = document.getElementById('firstname').value;
+    // let lastName = document.getElementById('lastname').value;
     let password = event.target.value;
-    if (firstName.length > 0 && password.toUpperCase().includes(firstName.toUpperCase())) {
-        let msgFirstName = document.createElement("p");
-        let nodeFirstName = document.createTextNode("Please do not include your first name in your password");
-        msgFirstName.appendChild(nodeFirstName);
-        messagesContainer.appendChild(msgFirstName);
-    }
-    if (lastName.length > 0 && password.toUpperCase().includes(lastName.toUpperCase())) {
-        let msgLastName = document.createElement("p");
-        let nodeLastName = document.createTextNode("Please do not include your last name in your password");
-        msgLastName.appendChild(nodeLastName);
-        messagesContainer.appendChild(msgLastName);
-    }
+    // if (firstName.length > 0 && password.toUpperCase().includes(firstName.toUpperCase())) {
+    //     let msgFirstName = document.createElement("p");
+    //     let nodeFirstName = document.createTextNode("❌Please do not include your first name in your password");
+    //     msgFirstName.appendChild(nodeFirstName);
+    //     messagesContainer.appendChild(msgFirstName);
+    // }
+    // if (lastName.length > 0 && password.toUpperCase().includes(lastName.toUpperCase())) {
+    //     let msgLastName = document.createElement("p");
+    //     let nodeLastName = document.createTextNode("❌Please do not include your last name in your password");
+    //     msgLastName.appendChild(nodeLastName);
+    //     messagesContainer.appendChild(msgLastName);
+    // }
 
     // At least 6 alphanumerical characters long
     let regex1 = /^[A-Za-z0-9]{6,}$/;
@@ -28,26 +28,46 @@ document.getElementById('password').addEventListener("input", (event) => {
     
     if (regex1.test(password)) {
         let msg1 = document.createElement("p");
-        let node1 = document.createTextNode("At least 6 alphanumerical characters long");
+        let node1 = document.createTextNode("✅ At least 6 alphanumerical characters long");
         msg1.appendChild(node1);
         messagesContainer.appendChild(msg1);
     }
     if (regex2.test(password)) {
         let msg2 = document.createElement("p");
-        let node2 = document.createTextNode("At least one upper case letter");
+        let node2 = document.createTextNode("✅ At least one upper case letter");
         msg2.appendChild(node2);
         messagesContainer.appendChild(msg2);
     }
     if (regex3.test(password)) {
         let msg3 = document.createElement("p");
-        let node3 = document.createTextNode("At least one lower case letter");
+        let node3 = document.createTextNode("✅ At least one lower case letter");
         msg3.appendChild(node3);
         messagesContainer.appendChild(msg3);
     }
     if (regex4.test(password)) {
         let msg4 = document.createElement("p");
-        let node4 = document.createTextNode("At least one number");
+        let node4 = document.createTextNode("✅ At least one number");
         msg4.appendChild(node4);
         messagesContainer.appendChild(msg4);
+    }
+});
+
+document.getElementById('test-button').addEventListener("click", (event) => {
+    event.preventDefault();
+    let messagesContainer = document.getElementById('messages');
+    let firstName = document.getElementById('firstname').value;
+    let lastName = document.getElementById('lastname').value;
+    let password = document.getElementById('password').value;
+    if (firstName.length > 0 && password.toUpperCase().includes(firstName.toUpperCase())) {
+        let msgFirstName = document.createElement("p");
+        let nodeFirstName = document.createTextNode("❌ Please do not include your first name in your password");
+        msgFirstName.appendChild(nodeFirstName);
+        messagesContainer.insertBefore(msgFirstName, messagesContainer.childNodes[0]);
+    }
+    if (lastName.length > 0 && password.toUpperCase().includes(lastName.toUpperCase())) {
+        let msgLastName = document.createElement("p");
+        let nodeLastName = document.createTextNode("❌ Please do not include your last name in your password");
+        msgLastName.appendChild(nodeLastName);
+        messagesContainer.insertBefore(msgLastName, messagesContainer.childNodes[0]);
     }
 });
